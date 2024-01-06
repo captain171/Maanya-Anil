c6076b344f83311e7895df0de965a0585b8ec94e
captain171 <152792207+captain171@users.noreply.github.com>
2024-01-06 09:42:26 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,12 @@
+function myFunction() {
+  window.location.href = '/HTML/FoodChain/FoodChain-Copy.html';
+}
+
+function myFunctionDAP() {
+  window.location.href = 'http://localhost:8888/notebooks/Documents/1RVU23CSE243_MAANYA_A_FINAL.ipynb';
+}
+
+function myFunctionsports() {
+  window.location.href = '/HTML/sports code/HTML/sports.html';
+}
+  
